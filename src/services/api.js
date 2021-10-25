da8f36bab6bf94fc0a2ea0c304d3253b788ed3df
Joao Pedro de Oli. Lage <JoaoPedro.Lage@outlook.com>
2021-10-25 15:32:13 -0300

    Iniciando projeto em grupo, VQV

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 export async function getCategories() {
   // Implemente aqui
+  // 'https://api.mercadolibre.com/sites/MLB/categories';
+
 }
 
 export async function getProductsFromCategoryAndQuery(/* categoryId, query */) {
   // Implemente aqui! Quando o fizer, descomente os parâmetros que essa função recebe
+
 }
