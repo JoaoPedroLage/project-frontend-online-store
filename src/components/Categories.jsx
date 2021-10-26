42bb342ca7ab102b005fd6e2c38ecae0e6ba1301
Danielen Cestari Nunes <danielen.cestari@gmail.com>
2021-10-26 17:40:36 -0300

    Requisito 04, testado, aguarda aprovação

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-//import { BrowserRouter, Route } from 'react-router-dom';
 import { getCategories } from '../services/api';
 
 export default class Categories extends React.Component {
@@ -10,27 +9,48 @@ export default class Categories extends React.Component {
       categoriesList: [],
     };
 
-    componentDidMount = () => {
-      this.categoriesList();
-    };
-    
-    getCategories().then((callback) => this.setState({
-      categoriesList: callback()
-    })) 
+    this.onGetCategoriesList = this.onGetCategoriesList.bind(this);
+  }
+
+  componentDidMount() {
+    this.onGetCategoriesList();
+  }
+
+  async onGetCategoriesList() {
+    const categoriesList = await getCategories();
+    const categoryName = categoriesList.map((element) => (
+      (
+        <div key={ element.id }>
+          <label
+            key={ element.id }
+            htmlFor={ element.name }
+          >
+            <input
+              type="radio"
+              data-testid="category"
+              id={ element.id }
+              name={ element.name }
+              onChange={ this.handleCategoriesList }
+            />
+            { element.name }
+          </label>
+        </div>
+      )));
+    this.setState({
+      categoriesList: categoryName,
+    });
+  }
+
+  render() {
+    const { categoriesList } = this.state;
+
+    return (
+      <div>
+        <aside>
+          Categorias
+          {categoriesList}
+        </aside>
+      </div>
+    );
   }
 }
-/* 4. Liste as categorias de produtos disponíveis via API na página principal
-PRIORIDADE 1 - Listar filtros de categoria obtidos da API na tela principal, de listagem do produto. 
-A requisição da API para recuperar as categorias deve ser feita uma única vez após o carregamento da tela.
-
-Tela principal - Com a lista de categorias
-Observações técnicas
-
-Um endpoint da API do Mercado Livre retorna as categorias de produto disponíveis para busca. 
-Em momento posterior tais categorias serão usadas para filtrar a listagem de produtos. 
-Por hora, elas devem ser listadas na tela da listagem, conforme protótipo.
-
-Adicione o atributo data-testid com o valor category nos elementos que possuem os nomes das categorias
-O que será verificado:
-
-  - Exibe as categorias retornadas pela API na página de listagem de produtos */
